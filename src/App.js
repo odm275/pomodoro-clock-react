77347a6d4aed5e8c1719e0e7616f567e33bcd5fb
odm275 <pomejia@gmail.com>
2018-01-28 21:11:24 -0600

    timer off by one sec at first; added plus button for session

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import './App.css'
 class App extends Component {
   constructor(){
     super();
-    
     this.second = 1000;
     this.minute = this.second*60;
     this.state = {
@@ -17,26 +16,28 @@ class App extends Component {
     };
 
   this.start = this.start.bind(this);
+  this.plusTime = this.plusTime.bind(this);
+
   }
   start(e){
     e.preventDefault();
-    const goal = new Date().getTime() + this.state.session;
-    //  BEGIN COUNTDOWN
-    let x = setInterval(() => {
+    const goal = new Date().getTime() + this.state.session; 
+    let timer = () =>{
       let now = new Date().getTime();// New time that approaches goal second by second
       let distance = goal - now; //in Milliseconds
-      let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));      
-      let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-      let time = minutes + ': ' + seconds;
-      this.setState({time: time});
+      let time = timeFormat(distance);
+      this.setState({time});
       if (distance < 0) {
         clearInterval(x);
+      }
     }
-    },1000);
+    timer();
+    let x = setInterval(timer,1000);
   }
   plusTime(time){
-    
-  }
+  let add = this.state.session + this.minute;
+  this.setState({session:add});    
+}
   minusTime(time){
 
   }
@@ -45,21 +46,32 @@ class App extends Component {
     const times = [this.state.session
                   ,this.state.shortBreak
                   ,this.state.longBreak];
-    console.log(this.state.time);
     return (
 
       <div className="App">
         <h1>Pomodoro City</h1>
         {times.map(time=>{
-          return(<h1>{time}</h1>)
+          return(<h1>{timeFormat(time,true)}</h1>)
         })}
-        <h1>{this.state.time ? this.state.time: '...'}</h1>
+        <h1>{this.state.time ? this.state.time: timeFormat(this.state.session)}</h1>
         <button onClick={this.start}>Start</button>
-        <button>+</button>
+        <button onClick={this.plusTime}>+</button>
         <button>-</button>
       </div>
     );
   }
+}
+
+
+function timeFormat(milliseconds, bool){
+  const minutes = Math.floor((milliseconds % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((milliseconds % (1000 * 60)) / 1000);      
+  if(bool){
+    return minutes;
+  }else{
+    return minutes + ': ' + seconds;
+  }
+
 }
 
 export default App;
